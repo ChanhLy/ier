8229089cb00a3af5b36a32bd3a08f00e3f39137b
Chanh Ly <chanhly.vn@gmail.com>
2020-09-22 19:46:17 +0700

    working

--- client/src/samples/components/CreateSamplesTable.tsx
+++ client/src/samples/components/CreateSamplesTable.tsx
@@ -1,17 +1,17 @@
 import { Table } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 import React from 'react';
-import { Experiment, Sample } from '..';
+import { Sample } from '..';
 import {
-  AMOUNT,
-  NOTE,
-  SAMPLE_DESCRIPTION,
-  SAMPLE_SYMBOL,
-  SAMPLE_TYPE,
-  SAMPLING_LOCATION,
-  TARGET_AND_METHOD,
-  UNIT,
-} from '../../utils/constants';
+  amountColumn,
+  descriptionColumn,
+  experimentsColumn,
+  locationColumn,
+  noteColumn,
+  symbolColumn,
+  typeColumn,
+  unitColumn,
+} from './SampleTable';
 
 interface Props {
   dataSource: Sample[];
@@ -22,31 +22,12 @@ export function CreateSamplesTable(props: Props) {
 }
 
 const columns: ColumnsType<Sample> = [
-  { title: SAMPLE_SYMBOL, dataIndex: 'symbol' },
-  { title: SAMPLING_LOCATION, dataIndex: 'location' },
-  { title: SAMPLE_DESCRIPTION, dataIndex: 'description' },
-  { title: AMOUNT, dataIndex: 'amount' },
-  { title: UNIT, dataIndex: 'unit' },
-  { title: SAMPLE_TYPE, dataIndex: 'type' },
-  {
-    title: TARGET_AND_METHOD,
-    dataIndex: 'experiments',
-    render: (value: Experiment[], record: Sample, index: number) => {
-      return toTargetAndMethods(value);
-    },
-  },
-  { title: NOTE, dataIndex: 'note' },
+  symbolColumn,
+  locationColumn,
+  descriptionColumn,
+  amountColumn,
+  unitColumn,
+  typeColumn,
+  experimentsColumn,
+  noteColumn,
 ];
-
-export function toTargetAndMethods(value: Experiment[]) {
-  return (
-    value
-      .map((experiment) => {
-        console.log(experiment);
-        const target = experiment.target;
-        const methods = experiment.methods?.join(',');
-        return target + (methods ? ` (${methods})` : '');
-      })
-      .join(', ') + ` (${value.length} chỉ tiêu)`
-  );
-}
