ada47030cd0f560c0f8e6a236f5b0d769954aad1
Chanh Ly <chanhly.vn@gmail.com>
2020-10-26 19:39:59 +0700

    working

---
+++ client/src/experiments/components/ExperimentTable.tsx
@@ -0,0 +1,8 @@
+import { Table } from 'antd';
+import React from 'react';
+
+export function ExperimentTable() {
+  return <Table></Table>;
+}
+
+const experimentColumns = [];
--- client/src/experiments/index.ts
+++ client/src/experiments/index.ts
@@ -1,1 +1,10 @@
+import { Sample } from '../samples';
+
 export { ExperimentRoutes } from './components/ExperimentRoutes';
+
+export interface Experiment {
+  _id: string;
+  sample: string | Sample;
+  target: string;
+  methods: string[];
+}
