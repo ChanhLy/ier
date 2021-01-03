08b3fce496464ff79d01887f93b4473294541cc5
chanhly <lytrungchanh@vietbando.com>
2021-01-03 23:55:38 +0700

    working

--- server/src/main/samples/samples.service.ts
+++ server/src/main/samples/samples.service.ts
@@ -1,16 +1,26 @@
 import dayjs from 'dayjs';
+import * as ExcelJS from 'exceljs';
+import { NOT_FOUND } from 'http-status';
 import { FilterQuery, QueryFindOptions } from 'mongoose';
-import { Experiment } from '../experiments/experiments.model';
+import { ContractDocument } from '../contracts';
+import { CustomerDocument } from '../customers/customers.model';
+import { Experiment, ExperimentDocument } from '../experiments/experiments.model';
 import { Sample, SampleBase, SampleDocument } from './samples.model';
 
 export class SampleService {
-  async returnedContract(id: any) {
+  async returnedContract(id: string): Promise<SampleDocument> {
     const sample = await this.findSampleById(id);
-    return sample && sample.update({ returned: !sample.returned }).exec();
+    if (!sample) {
+      throw NOT_FOUND;
+    }
+    return sample.update({ returned: !sample.returned }).exec();
   }
-  async paidContract(id: any) {
+  async paidContract(id: string): Promise<SampleDocument> {
     const sample = await this.findSampleById(id);
-    return sample && sample.update({ paid: !sample.paid }).exec();
+    if (!sample) {
+      throw NOT_FOUND;
+    }
+    return sample.update({ paid: !sample.paid }).exec();
   }
   async deleteSampleById(_id: string): Promise<number> {
     return Sample.updateOne({ _id }, { deletedAt: new Date() }).exec();
@@ -75,5 +85,33 @@ export class SampleService {
     await Sample.updateMany(query, update).exec();
 
     return true;
+  }
+
+  setFormData(
+    workbook: ExcelJS.Workbook,
+    sample: SampleDocument,
+    customer: CustomerDocument,
+    contract: ContractDocument,
+    experiments: ExperimentDocument[]
+  ): void {
+    const sheet = workbook.getWorksheet(1);
+    sheet.getCell('Q5').value = sample.id;
+    sheet.getCell('D8').value = sample.location;
+    sheet.getCell('D9').value = customer.address;
+    sheet.getCell('D10').value = '';
+    sheet.getCell('Q8').value = dayjs(contract.sampleReceivedDate).format('DD/MM/YYYY');
+    sheet.getCell('Q9').value = sample.type;
+
+    const today = new Date();
+    sheet.getCell('L45').value = `Tp.HCM, ngày ${today.getDay()} tháng ${today.getMonth()} năm ${today.getFullYear()}`;
+
+    experiments.forEach((experiment, i) => {
+      const row = i * 2 + 13;
+      sheet.getCell(`A${row}`).value = i;
+      sheet.getCell(`B${row}`).value = experiment.target;
+      sheet.getCell(`F${row}`).value = experiment.unit;
+      sheet.getCell(`H${row}`).value = experiment.result;
+      sheet.getCell(`L${row}`).value = experiment.methods.join(', ');
+    });
   }
 }
